51e41e96efdc65daf54a984f7f79f66e150ffe2b
snowmerak <snowmerak@outlook.com>
2022-04-08 23:11:04 +0900

    add NumberConv

--- compile/backend/kotlin/conv.go
+++ compile/backend/kotlin/conv.go
@@ -6,6 +6,34 @@ func NameConv(in string) string {
 	return strings.Title(in)
 }
 
+func NumberConv(unsigned bool, bit int) string {
+	switch unsigned {
+	case true:
+		switch bit {
+		case 8:
+			return "UByte"
+		case 16:
+			return "UShort"
+		case 32:
+			return "UInt"
+		case 64:
+			return "ULong"
+		}
+	case false:
+		switch bit {
+		case 8:
+			return "Byte"
+		case 16:
+			return "Short"
+		case 32:
+			return "Int"
+		case 64:
+			return "Long"
+		}
+	}
+	return "ULong"
+}
+
 func TypeConv(in string) string {
 	switch in {
 	case "uint8":
