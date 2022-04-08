0ec58056e319ba66ce9204822d359e19b17864ea
snowmerak <snowmerak@outlook.com>
2022-04-08 23:11:16 +0900

    create enum code

---
+++ compile/backend/kotlin/enum.go
@@ -0,0 +1,18 @@
+package kotlin
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/lemon-mint/vstruct/ir"
+)
+
+func writeEnums(w io.Writer, i *ir.IR) {
+	for _, e := range i.Enums {
+		fmt.Fprintf(w, "public enum class %s(val index: %s) {\n", NameConv(e.Name), NumberConv(true, e.Size*8))
+		for i, o := range e.Options {
+			fmt.Fprintf(w, "\t%s(%d),\n", NameConv(o), i)
+		}
+		fmt.Fprintf(w, "}\n\n")
+	}
+}
