0b91461c21f89bcc54adf0eaea63c0cb86c4b1d4
snowmerak <snowmerak@outlook.com>
2022-04-09 16:36:03 +0900

    add index

--- compile/backend/kotlin/enum.go
+++ compile/backend/kotlin/enum.go
@@ -11,7 +11,7 @@ func writeEnums(w io.Writer, i *ir.IR) {
 	for _, e := range i.Enums {
 		fmt.Fprintf(w, "public enum class %s(val index: %s) {\n", NameConv(e.Name), NumberConv(true, e.Size*8))
 		for i, o := range e.Options {
-			fmt.Fprintf(w, "\t%s(%d),\n", NameConv(o), i)
+			fmt.Fprintf(w, "\t%s(index = %du),\n", NameConv(o), i)
 		}
 		fmt.Fprintf(w, "}\n\n")
 	}
