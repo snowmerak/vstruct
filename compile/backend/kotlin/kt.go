0a0664a929ce806b2c479bbcbb52e285da3d86bb
snowmerak <snowmerak@outlook.com>
2022-04-09 14:33:06 +0900

    Create kt.go

---
+++ compile/backend/kotlin/kt.go
@@ -0,0 +1,27 @@
+package kotlin
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/lemon-mint/vstruct/ir"
+)
+
+func Generate(w io.Writer, i *ir.IR, packageName string) error {
+	var codedataBuf strings.Builder
+	writeAliases(&codedataBuf, i)
+	writeEnums(&codedataBuf, i)
+	writeStructs(&codedataBuf, i)
+	output := fmt.Sprintf(
+		`package vstruct.%s
+{
+%s
+}
+`,
+		packageName,
+		codedataBuf.String(),
+	)
+	_, err := w.Write([]byte(output))
+	return err
+}
